6d8d21517dfa0d2c65482a17476b3edf4bd73cde
Ilya Wise <ilyaphotoshop17@gmail.com>
2019-10-22 13:29:27 +0300

    local com desktop

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ function getValue() {
     let input = document.querySelector('#OneLabInp').value;
     let sum = input.split('',3);
 
-    sum.forEach(() => {
-        return sum
+    sum.forEach((sum) => {
+        return sum[0] + sum[1] + sum[2]
+
     });
+    console.log(sum)
 
     // for(let num of sum) {
     //     let inner = sum.map((sum) =>{
@@ -20,3 +22,8 @@ function getValue() {
 
     console.log(sum);
 }
+
+
+let mus = [1,2,3]
+let result = mus[0] + mus[1] + mus[2];
+

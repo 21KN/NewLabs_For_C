89778000b4214870e36c5b41b40e7750f83cb4a7
Ilya Wise <ilyaphotoshop17@gmail.com>
2019-10-22 20:33:48 +0300

    local com

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-<<<<<<< HEAD
+
     function getValueLabOne() {
         let input = document.querySelector('#OneLabInp').value;
         let sum = input.split('', 3);
@@ -50,48 +50,40 @@
 
         console.log(arr, maxValue,span)
     }
-
-=======
-function getValue() {
-    let input = document.querySelector('#OneLabInp').value;
-    let sum = input.split('', 3);
-    let arr = [
-        parseInt(sum[0],),
-        parseInt(sum[1],),
-        parseInt(sum[2],),
-    ];
-
-<<<<<<< HEAD
-    sum.forEach((sum) => {
-        return sum[0] + sum[1] + sum[2]
-
-    });
-    console.log(sum)
-=======
-    console.log(`${arr[0]} + ${arr[1]} + ${arr[2]} = `,arr[0] + arr[1] + arr[2]);
-    let spanT = document.querySelector('#spanTextValue');
-    spanT.innerHTML = `${arr[0]} + ${arr[1]} + ${arr[2]} = ${arr[0] + arr[1] + arr[2]}`;
->>>>>>> 0d466e5b0488814ce0aca2ba701b31d2788a7465
-
-    if(input.length > 3) {
-        console.error('Value > 3');
-        spanT.innerHTML = `<span id="error">Помилка</span>: значення бiльше <u>3</u>!`
-    }
-
-<<<<<<< HEAD
-
-    console.log(`${arr[0]} + ${arr[1]} + ${arr[2]} = `,arr[0] + arr[1] + arr[2]);
-    let span = document.querySelector('#spanTextValue');
-    span.innerHTML = `${arr[0]} + ${arr[1]} + ${arr[2]} = ${arr[0] + arr[1] + arr[2]}`;
-=======
-    // if (typeof arr[0] === 'number' && 'NaN') {
-    //     console.error('Тип данных undefined');
-    //     spanT.innerHTML = `<span id="error">Помилка</span>: тип данних undefuned`
-    // }
->>>>>>> 0d466e5b0488814ce0aca2ba701b31d2788a7465
-}
->>>>>>> 49810c5c6f29a99eb45b31aa6877c6371dee8b79
-
+//
+// function getValue() {
+//     let input = document.querySelector('#OneLabInp').value;
+//     let sum = input.split('', 3);
+//     let arr = [
+//         parseInt(sum[0],),
+//         parseInt(sum[1],),
+//         parseInt(sum[2],),
+//     ];
+//
+//     sum.forEach((sum) => {
+//         return sum[0] + sum[1] + sum[2]
+//
+//     });
+//     console.log(sum)
+//     console.log(`${arr[0]} + ${arr[1]} + ${arr[2]} = `,arr[0] + arr[1] + arr[2]);
+//     let spanT = document.querySelector('#spanTextValue');
+//     spanT.innerHTML = `${arr[0]} + ${arr[1]} + ${arr[2]} = ${arr[0] + arr[1] + arr[2]}`;
+//
+//     if(input.length > 3) {
+//         console.error('Value > 3');
+//         spanT.innerHTML = `<span id="error">Помилка</span>: значення бiльше <u>3</u>!`
+//     }
+//
+//
+//     console.log(`${arr[0]} + ${arr[1]} + ${arr[2]} = `,arr[0] + arr[1] + arr[2]);
+//     let span = document.querySelector('#spanTextValue');
+//     span.innerHTML = `${arr[0]} + ${arr[1]} + ${arr[2]} = ${arr[0] + arr[1] + arr[2]}`;
+//     // if (typeof arr[0] === 'number' && 'NaN') {
+//     //     console.error('Тип данных undefined');
+//     //     spanT.innerHTML = `<span id="error">Помилка</span>: тип данних undefuned`
+//     // }
+// }
+//
 
 
 

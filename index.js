0d466e5b0488814ce0aca2ba701b31d2788a7465
Ilya <ilyaphotoshop17@gmail.com>
2019-10-22 13:32:30 +0300

    last change

--- index.js
+++ index.js
@@ -8,12 +8,18 @@ function getValue() {
         parseInt(sum[2],),
     ];
 
+    console.log(`${arr[0]} + ${arr[1]} + ${arr[2]} = `,arr[0] + arr[1] + arr[2]);
+    let spanT = document.querySelector('#spanTextValue');
+    spanT.innerHTML = `${arr[0]} + ${arr[1]} + ${arr[2]} = ${arr[0] + arr[1] + arr[2]}`;
+
     if(input.length > 3) {
-        console.error('Value > 3') // дописать проверку на количество вводимых данных
+        console.error('Value > 3');
+        spanT.innerHTML = `<span id="error">Помилка</span>: значення бiльше <u>3</u>!`
     }
 
-    console.log(`${arr[0]} + ${arr[1]} + ${arr[2]} = `,arr[0] + arr[1] + arr[2]);
-    let span = document.querySelector('#spanTextValue');
-    span.innerHTML = `${arr[0]} + ${arr[1]} + ${arr[2]} = ${arr[0] + arr[1] + arr[2]}`;
+    // if (typeof arr[0] === 'number' && 'NaN') {
+    //     console.error('Тип данных undefined');
+    //     spanT.innerHTML = `<span id="error">Помилка</span>: тип данних undefuned`
+    // }
 }
 

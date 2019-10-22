9fa4001ae7d54f690ad96d0c9e9489ed0c4a7862
Ilya <ilyaphotoshop17@gmail.com>
2019-10-22 10:13:50 +0300

    #1 work

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 
 function getValue() {
     let input = document.querySelector('#OneLabInp').value;
-    let sum = input.split('', 3); // НЕ ПОДХОДИТ для перебора в намбер
-    let sumNew = Number(sum);
+    let sum = input.split('', 3);
+    let arr = [
+        parseInt(sum[0],10),
+        parseInt(sum[1],10),
+        parseInt(sum[2],10),
+    ];
+    console.log(`${arr[0]} + ${arr[1]} + ${arr[2]} = `,arr[0] + arr[1] + arr[2]);
 
-    // {
-    //     one: sum[0],
-    //     two: sum[1],
-    //     there: sum[2],
-    // };
-
-    console.log(sumNew);
-
-    sum.forEach(number => number );
+    // sum.forEach(number => number );
 
     // sum.forEach((index) => {
     //     return
@@ -29,7 +26,7 @@ function getValue() {
     //     return sum + result
     // });
 
-    console.log(sum)
+    // console.log(sum)
     // console.log(sum[0] + sum[1] + sum[2]);
 }
 

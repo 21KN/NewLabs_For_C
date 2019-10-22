bfa864c707585f1f8900f0405fd5053748f42a32
Ilya <ilyaphotoshop17@gmail.com>
2019-10-22 11:06:06 +0300

    add new js/html/css

--- index.js
+++ index.js
@@ -3,30 +3,17 @@ function getValue() {
     let input = document.querySelector('#OneLabInp').value;
     let sum = input.split('', 3);
     let arr = [
-        parseInt(sum[0],10),
-        parseInt(sum[1],10),
-        parseInt(sum[2],10),
+        parseInt(sum[0],),
+        parseInt(sum[1],),
+        parseInt(sum[2],),
     ];
+
+    if(input.length > 3) {
+        console.error('Value > 3') // дописать проверку на количество вводимых данных
+    }
+
     console.log(`${arr[0]} + ${arr[1]} + ${arr[2]} = `,arr[0] + arr[1] + arr[2]);
-
-    // sum.forEach(number => number );
-
-    // sum.forEach((index) => {
-    //     return
-    // });
-
-    // for(let num of sum) {
-    //     let inner = sum.map((sum) =>{
-    //         return  num[0] + num[1] + num[2];
-    //     });
-    //     console.log(inner, sum, num)
-    // }
-
-    // let result = sum.reduce( (sum,result) => {
-    //     return sum + result
-    // });
-
-    // console.log(sum)
-    // console.log(sum[0] + sum[1] + sum[2]);
+    let span = document.querySelector('#spanTextValue');
+    span.innerHTML = `${arr[0]} + ${arr[1]} + ${arr[2]} = ${arr[0] + arr[1] + arr[2]}`;
 }
 

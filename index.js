9488ad61f4ccc57fc420be2f097039aa556c1798
ArtMingo <38019101+WiseChannel@users.noreply.github.com>
2019-10-22 17:29:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-<<<<<<< HEAD
+
     function getValueLabOne() {
         let input = document.querySelector('#OneLabInp').value;
         let sum = input.split('', 3);
@@ -51,7 +51,7 @@
         console.log(arr, maxValue,span)
     }
 
-=======
+
 function getValue() {
     let input = document.querySelector('#OneLabInp').value;
     let sum = input.split('', 3);
@@ -61,36 +61,35 @@ function getValue() {
         parseInt(sum[2],),
     ];
 
-<<<<<<< HEAD
     sum.forEach((sum) => {
         return sum[0] + sum[1] + sum[2]
 
     });
     console.log(sum)
-=======
+
     console.log(`${arr[0]} + ${arr[1]} + ${arr[2]} = `,arr[0] + arr[1] + arr[2]);
     let spanT = document.querySelector('#spanTextValue');
     spanT.innerHTML = `${arr[0]} + ${arr[1]} + ${arr[2]} = ${arr[0] + arr[1] + arr[2]}`;
->>>>>>> 0d466e5b0488814ce0aca2ba701b31d2788a7465
+
 
     if(input.length > 3) {
         console.error('Value > 3');
         spanT.innerHTML = `<span id="error">Помилка</span>: значення бiльше <u>3</u>!`
     }
 
-<<<<<<< HEAD
+
 
     console.log(`${arr[0]} + ${arr[1]} + ${arr[2]} = `,arr[0] + arr[1] + arr[2]);
     let span = document.querySelector('#spanTextValue');
     span.innerHTML = `${arr[0]} + ${arr[1]} + ${arr[2]} = ${arr[0] + arr[1] + arr[2]}`;
-=======
+
     // if (typeof arr[0] === 'number' && 'NaN') {
     //     console.error('Тип данных undefined');
     //     spanT.innerHTML = `<span id="error">Помилка</span>: тип данних undefuned`
     // }
->>>>>>> 0d466e5b0488814ce0aca2ba701b31d2788a7465
+
 }
->>>>>>> 49810c5c6f29a99eb45b31aa6877c6371dee8b79
+
 
 
 
